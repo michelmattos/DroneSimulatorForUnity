ea397df490d7792d5a569be4efebe54d855ae430
Michel Mattos <mattos.michel@gmail.com>
2014-10-27 14:06:18 -0200

    Created a new script to control the drone

---
+++ src/Assets/Scripts/Drone/MotorThrust.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotorThrust : MonoBehaviour {
+	
+	public float MaxThrust = .5f;
+	public float Thrust = 0f;
+
+	private Rigidbody rigid;
+
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody>();
+	}
+	
+	public void ApplyTorque(float percent) {
+		Thrust = Mathf.Clamp01(percent) * MaxThrust;
+		rigid.AddForce(transform.up * (Thrust * Physics.gravity.magnitude));
+	}
+	
+}
